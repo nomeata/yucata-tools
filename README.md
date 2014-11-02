7740f8392989f791d2abd31138e2338387417df6
Joachim Breitner <mail@joachim-breitner.de>
2014-11-02 16:15:30 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,45 @@
+yucata-tools
+============
+
+This repository contains some inofficial tools that might be convenient for the
+heavy user of the great and free online boardgame platform [yucata.de].
+
+Use at your own risk!
+
+These tools do not use official APIs, so they are likely to break regularly
+when the server changes.
+
+Features
+--------
+
+Currently, yucata-tools has only one feature: Opening the game you have to play
+next in the browser. To use it, simply start the provided python script:
+
+    $ ./yucata.py
+    Cannot fetch games. Trying to log in first.
+    Login: nomeata
+    Password: 
+    7 games are running, you can move at 1 games
+    You now have to move at a game of Russian Railroads.
+    Opening http://www.yucata.de/de/Game/4619291...
+    
+At this point, the browser will open this game. If there is no game where you
+have to do a move, nothing happens.
+
+The script will store the cookie, so the next time you will not have to log in
+again. It does not store the password, though, so when the cookie expires, you
+will be asked again for the password.
+
+TODO
+----
+
+The code is not the cleanest. But for this simple task, that does not matter.
+
+It has `/de/` hardcoded. If that bothers you, let me know.
+
+I’d like to turn this into a small app for the SailfishOS, so that I can use it
+on my [Jolla Phone].
+
+[yucata.de]: http://yucata.de/
+[Jolla Phone]: http://jolla.com/
+ 
---
+++ yucata.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+import mechanize
+import cookielib
+import xdg.BaseDirectory
+import os.path
+import ConfigParser
+import getpass
+import json
+import webbrowser
+
+# setup
+cookiefile = os.path.join(xdg.BaseDirectory.save_data_path("yucata-tools"), "cookies")
+cookiejar =cookielib.LWPCookieJar(cookiefile)
+try:
+    cookiejar.load(ignore_discard=True)
+except IOError:
+    pass
+
+br = mechanize.Browser()
+br.set_cookiejar(cookiejar)
+
+
+configfile = os.path.join(xdg.BaseDirectory.save_config_path("yucata-tools"), "config.ini")
+config = ConfigParser.RawConfigParser()
+config.read(configfile)
+
+if not config.has_section('Login'):
+    config.add_section('Login')
+
+login = None
+if config.has_option('Login', 'username'):
+    login = config.get('Login', 'username')
+
+
+def get_cookie():
+    prompt = "Login [%s]: " % login if login else "Login: "
+    thelogin = raw_input(prompt)
+    if thelogin:
+        login = thelogin
+    password = getpass.getpass()
+
+    config.set('Login', 'username', login)
+    config.write(file(configfile, 'wb'))
+
+    br.open("http://www.yucata.de/de")
+    br.select_form('aspnetForm')
+    br.form["ctl00$ctl07$edtLogin"] = login
+    br.form["ctl00$ctl07$edtPassword"] = password
+    br.submit(name="ctl00$ctl07$btnLogin", label="Anmelden")
+    cookiejar.save(ignore_discard=True)
+
+def try_get_games():
+    try:
+        resp = br.open("http://www.yucata.de/Services/YucataService.svc/GetCurrentGames", data="")
+        return resp.read()
+    except mechanize.HTTPError, e:
+        if int(e.code) == 500:
+            return None
+        else:
+            raise
+        
+def get_games():
+    games = try_get_games()
+    if games is None:
+        print "Cannot fetch games. Trying to log in first."
+        get_cookie()
+        games = try_get_games()
+    if games is None:
+        print "Sorry, cannot fetch games"
+        return
+
+    data = json.loads(games)
+    #print json.dumps(data, sort_keys=True, indent=4, separators=(',', ': '))
+
+    games = data["d"]["Games"]
+    games_on_turn = []
+    next_game = None
+
+    next_game_id = data["d"]["NextGameOnTurn"]
+
+    for g in games:
+        on_turn = g["PlayerOnTurn"]
+        for p in g["Players"]:
+            if p["PlayerID"] == on_turn and p["Login"] == login:
+                games_on_turn.append(g)
+        if g["ID"] == next_game_id:
+            next_game = g
+
+    print "%d games are running, you can move at %d games" % (len(games), len(games_on_turn))
+    if next_game:
+        print "You now have to move at a game of %s." % next_game["GameName"]
+        url = "http://www.yucata.de/de/Game/%d" % next_game_id
+        print "Opening %s..." % url
+        webbrowser.open(url)
+
+get_games()
